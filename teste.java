397ea1041b2bd1d5a5078066e261e767fa3f58a6
Davi <davibbraga@outlook.com>
2022-05-25 16:48:13 -0300

    Adicionando fibonacci

--- teste.java
+++ teste.java
@@ -1,15 +1,13 @@
 public class teste{
 
 	public static void main(String[] args){
-		System.out.println("Ola mundo!");
-		int num=5;
-		for(int i=0;i<num;i++){
-			System.out.println("Impar!");
-			if(i%2==0){
-				System.out.println("Par!");
-			}
+		System.out.println("Ola mundo!\n");
+		System.out.println("Digite um número: \t");
+		int num=MyIO.readInt();
+		for(int i=0,j=0,k=1;i<num;i++,j=k,k+=j){
+			System.out.println(k+"\n");
 		}
-		System.out.println("Tchau Mundo!");
+		System.out.println("Tchau Mundo!\n");
 	}
 }
 
