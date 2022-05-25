c354e3e2e16d5aa9e8f44b11b2c47aa0d5ef4dc8
Davi <davibbraga@outlook.com>
2022-05-25 16:12:14 -0300

    inserindo teste.java

---
+++ teste.java
@@ -0,0 +1,15 @@
+public class teste{
+
+	public static void main(String[] args){
+		System.out.println("Ola mundo!");
+		int num=5;
+		for(int i=0;i<num;i++){
+			System.out.println("Impar!");
+			if(i%2==0){
+				System.out.println("Par!");
+			}
+		}
+		System.out.println("Tchau Mundo!");
+	}
+}
+
